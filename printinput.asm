7415d0774d8c3c4974c02cf3a95430f7ef6b7a33
fumiama <s862105088@gmail.com>
2021-03-31 14:10:29 +0800

    调整按键

--- printinput.asm
+++ printinput.asm
@@ -10,24 +10,27 @@
 main:
 *   jsr getin
     beq skip
+    cmp #'@
+    beq end
     jsr printbyte
     lda #13		; 换行
     jsr chrout
 skip:
     jmp -
+end:
+    rts
 
 printbyte:
     sta _na
     txa
     pha
-    ldx #7	; 打印8bit
+    ldx #8	    ; 打印8bit
 *   lda #$30	; a = '0'
     asl _na		; 左移一位，溢出到c
-    bcc +		; if(c == 0) goto 下一个星号
-    adc #0		; else a = a + c + 0
-*   jsr chrout	; putchar(a)
+    adc #0		; a = a + c + 0
+    jsr chrout	; putchar(a)
     dex			; x--
-    bne --		; if(x != 0) goto 上两个星号
+    bne -		; if(x != 0) goto 上个星号
     pla
     tax
     rts
